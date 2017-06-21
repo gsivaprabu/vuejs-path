17803fd8f3a8709f21e70a29c4b3fca9f4882afb
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2017-06-21 05:54:45 +0530

    Creating VueJs Templates - Binding to CSS Classes

---
+++ 01 Vuejs Getting Started/03 Creating VueJs Templates/15 Binding to CSS Classes/Readme.md
@@ -0,0 +1,8 @@
+# 15 Creating VueJs Templates - Binding to CSS Classes
+
+- Helps you maintain separation between your design and data
+- CSS class flase means not reflected.
+- Get CSS Classes from an Object
+- Get CSS Classes from an Array
+- Bind to specific CSS properties
+- Directives
---
+++ 01 Vuejs Getting Started/03 Creating VueJs Templates/15 Binding to CSS Classes/script.js
@@ -0,0 +1,17 @@
+var myProfile = new Vue({
+    el: '#profile',
+    data: {
+        profileName: 'Sivaprabu Ganesan',
+        profileLogo: '../../00 libraries/images/gsivaprabu.jpg',
+        profileDesignation: 'Senior Software Engineer',
+        color: '#3BAF12',
+        accentColor: {
+            color: '#FF6A00',
+        },
+        headers: {
+            fontFamily: 'Verdana',
+            margin: 0
+        },
+        isOnline: true
+    }
+});
