368f51eebfb1d56369779aaaeed95f2d165c6038
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2017-06-22 07:04:19 +0530

    Responding to User Events in Vuejs - Using Event Handlers

---
+++ 01 Vuejs Getting Started/05 Responding to User Events in Vuejs/02 Using Event Handlers/README.md
@@ -0,0 +1,7 @@
+# 02 Responding to User Events in Vuejs - Using Event Handlers
+
+- Attaching to Events
+	- v-on Directive
+		- Listens for the events in the HTML DOM
+		- Traditional HTML we used **onClick**
+		- Ignore the “on” prefix that you might’ve used in HTML
---
+++ 01 Vuejs Getting Started/05 Responding to User Events in Vuejs/02 Using Event Handlers/script.js
@@ -0,0 +1,30 @@
+function checkForEnter(e) {
+    var code = (e.keyCode ? e.keyCode : e.which);
+    if (code == 13) {
+        growler.executeSearch();
+        return false;
+    }
+    return true;
+}
+
+var growler = new Vue({
+    el: '#growler',
+    data: {
+        query: '',
+        history: ''
+    },
+    methods: {
+        evaluateKey: function(e) {
+            var code = (e.keyCode ? e.keyCode : e.which);
+            this.history += 'key clicked: "' + e.key + '" (' + code + ')\n';
+        },
+        executeSearch: function(e) {
+            if (this.query) {
+                this.history += 'Search button clicked. Query "' + this.query + '" was entered.\n';
+            }
+        },
+        clearHistory: function() {
+            this.history = '';
+        }
+    }
+});
