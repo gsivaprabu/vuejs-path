1b9f0a8464f790821fb280eb2227e9bf45b0fc2b
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2017-07-07 09:05:24 +0530

    Conditional Rendering and Rendering of Lists - Traversing Object Properties

---
+++ 01 Vuejs Getting Started/06 Conditional Rendering and Rendering of Lists/07 Traversing Object Properties/README.md
@@ -0,0 +1,8 @@
+# 07 Conditional Rendering and Rendering of Lists - Traversing Object Properties
+
+
+- In the context of properties, the alias value starts at 0, not 1
+
+### Iterating Over an Object
+	- Gets keys via Object.keys()
+	- Different JavaScript engines return properties in different orders
---
+++ 01 Vuejs Getting Started/06 Conditional Rendering and Rendering of Lists/07 Traversing Object Properties/alias.js
@@ -0,0 +1,14 @@
+vargrowler = new Vue({
+    el: '#growler',
+    data: {
+        currentUser: {
+            firstName: 'Chad',
+            fingers: 10,
+            tags: ['male', 'scorpio'],
+            socialMedia: {
+                twitter: '@ecofic',
+                youtube: 'ecofic'
+            }
+        }
+    }
+});
