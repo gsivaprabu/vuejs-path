bab330dcf011e5f94629ba91b63780302d398ef9
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2020-01-05 18:03:51 +0530

    Creating Custom Date Filters

--- 01 Vuejs_Getting Started  2019/pappa/05-interacting-within-a-component/begin/vue-heroes/src/components/heroes.vue
+++ 01 Vuejs_Getting Started  2019/pappa/05-interacting-within-a-component/begin/vue-heroes/src/components/heroes.vue
@@ -33,41 +33,69 @@
             <div class="content">
               <div class="field">
                 <label class="label" for="id">id</label>
-                <label class="input" id="id" readonly>
-                  {{
+                <label class="input" id="id" readonly>{{
                   selectedHero.id
-                  }}
-                </label>
+                }}</label>
               </div>
               <div class="field">
                 <label class="label" for="firstName">first name</label>
-                <input class="input" id="firstName" v-model="selectedHero.firstName" />
+                <input
+                  class="input"
+                  id="firstName"
+                  v-model="selectedHero.firstName"
+                />
               </div>
               <div class="field">
                 <label class="label" for="lastName">last name</label>
-                <input class="input" id="lastName" v-model="selectedHero.lastName" />
+                <input
+                  class="input"
+                  id="lastName"
+                  v-model="selectedHero.lastName"
+                />
               </div>
               <div class="field">
                 <label class="label" for="description">description</label>
-                <input class="input" id="description" v-model="selectedHero.description" />
-              </div>
-              <div class="field">
-                <label class="label" for="capeCounter">Cape Counter</label>
                 <input
                   class="input"
-                  id="capeCounter"
+                  id="description"
+                  v-model="selectedHero.description"
+                />
+              </div>
+              <div class="field">
+                <label class="label" for="originDate">origin date</label>
+                <input
+                  type="date"
+                  class="input"
+                  id="originDate"
+                  v-model="selectedHero.originDate"
+                />
+                <p class="comment">
+                  My origin story began on
+                  {{ selectedHero.originDate | shortDate }}
+                </p>
+              </div>
+              <div class="field">
+                <label class="label" for="capeCounter">cape counter</label>
+                <input
+                  class="input"
+                  name="capeCounter"
                   type="number"
                   v-model="selectedHero.capeCounter"
                 />
               </div>
               <div class="field">
-                <label class="label" for="capeMessage">Cape Message</label>
-                <label class="input" name="capeMessage">{{capeMessage}}</label>
+                <label class="label" for="capeMessage">cape message</label>
+                <label class="input" name="capeMessage">{{
+                  capeMessage
+                }}</label>
               </div>
             </div>
           </div>
           <footer class="card-footer">
-            <button class="link card-footer-item cancel-button" @click="cancelHero()">
+            <button
+              class="link card-footer-item cancel-button"
+              @click="cancelHero()"
+            >
               <i class="fas fa-undo"></i>
               <span>Cancel</span>
             </button>
@@ -83,12 +111,17 @@
 </template>
 
 <script>
+import { format } from 'date-fns';
+const inputDateFormat = 'YYYY-MM-DD';
+const displayDateFormat = 'MMM DD, YYYY';
+
 const ourHeroes = [
   {
     id: 10,
     firstName: 'Ella',
     lastName: 'Papa',
     capeCounter: 1,
+    originDate: format(new Date(1996, 5, 1), inputDateFormat),
     description: 'fashionista',
   },
   {
@@ -96,6 +129,7 @@ const ourHeroes = [
     firstName: 'Madelyn',
     lastName: 'Papa',
     capeCounter: 3,
+    originDate: format(new Date(1998, 7, 1), inputDateFormat),
     description: 'the cat whisperer',
   },
   {
@@ -103,6 +137,7 @@ const ourHeroes = [
     firstName: 'Haley',
     lastName: 'Papa',
     capeCounter: 2,
+    originDate: format(new Date(1999, 8, 1), inputDateFormat),
     description: 'pen wielder',
   },
   {
@@ -110,6 +145,7 @@ const ourHeroes = [
     firstName: 'Landon',
     lastName: 'Papa',
     capeCounter: 0,
+    originDate: format(new Date(2000, 9, 1), inputDateFormat),
     description: 'arc trooper',
   },
 ];
@@ -137,10 +173,9 @@ export default {
         setTimeout(() => resolve(ourHeroes), 1500);
       });
     },
-
     async loadHeroes() {
-      this.Heroes = [];
-      this.message = 'getting the heroes. Please be patient';
+      this.heroes = [];
+      this.message = 'getting the heroes, please be patient';
       this.heroes = await this.getHeroes();
       this.message = '';
     },
@@ -176,11 +211,16 @@ export default {
   watch: {
     'selectedHero.capeCounter': {
       immediate: true,
-      deep: true,
       handler(newValue, oldValue) {
-        console.log(`Watcher evaluated. old=${oldValue},new=${newValue}`);
+        console.log(`Watcher evalauted. old=${oldValue}, new=${newValue}`);
         this.handleTheCapes(newValue);
       },
+    },
+  },
+  filters: {
+    shortDate: function(value) {
+      console.log('Date mismatch debug ' ,value)
+      return format(value, displayDateFormat);
     },
   },
 };
