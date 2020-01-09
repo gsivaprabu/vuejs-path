98b320c4ed462b0b54be23170431476d4cf7d885
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2020-01-09 09:14:19 +0530

    Passing Objects in Props

--- 01 Vuejs_Getting Started  2019/06 Component Communication/05 Passing Objects in Props/README.md
+++ 01 Vuejs_Getting Started  2019/06 Component Communication/05 Passing Objects in Props/README.md
@@ -1,1 +1,6 @@
 # 05 Passing Objects in Props
+
+- Clone the data top to botoom
+- Child updated parent also affected so need to clone the data
+- **Shallow cloning in JavaScript**
+- 
--- 01 Vuejs_Getting Started  2019/pappa/06-component-communication/begin/vue-heroes/src/components/hero-detail.vue
+++ 01 Vuejs_Getting Started  2019/pappa/06-component-communication/begin/vue-heroes/src/components/hero-detail.vue
@@ -10,33 +10,33 @@
             <label class="label" for="id">id</label>
             <label class="input" name="id" readonly>
               {{
-              hero.id
+              clonedHero.id
               }}
             </label>
           </div>
           <div class="field">
             <label class="label" for="firstName">first name</label>
-            <input class="input" name="firstName" v-model="hero.firstName" />
+            <input class="input" name="firstName" v-model="clonedHero.firstName" />
           </div>
           <div class="field">
             <label class="label" for="lastName">last name</label>
-            <input class="input" name="lastName" v-model="hero.lastName" />
+            <input class="input" name="lastName" v-model="clonedHero.lastName" />
           </div>
           <div class="field">
             <label class="label" for="description">description</label>
-            <input class="input" name="description" v-model="hero.description" />
+            <input class="input" name="description" v-model="clonedHero.description" />
           </div>
           <div class="field">
             <label class="label" for="originDate">origin date</label>
-            <input type="date" class="input" id="originDate" v-model="hero.originDate" />
+            <input type="date" class="input" id="originDate" v-model="clonedHero.originDate" />
             <p class="comment">
               My origin story began on
-              {{ hero.originDate | shortDate }}
+              {{ clonedHero.originDate | shortDate }}
             </p>
           </div>
           <div class="field">
             <label class="label" for="capeCounter">cape counter</label>
-            <input class="input" name="capeCounter" type="number" v-model="hero.capeCounter" />
+            <input class="input" name="capeCounter" type="number" v-model="clonedHero.capeCounter" />
           </div>
           <div class="field">
             <label class="label" for="capeMessage">cape message</label>
@@ -75,9 +75,17 @@ export default {
     },
   },
 
+  data() {
+    return {
+      clonedHero: { ...this.hero },
+    };
+  },
+
   computed: {
     fullName() {
-      return this.hero ? `${this.hero.firstName} ${this.hero.lastName}` : '';
+      return this.clonedHero
+        ? `${this.clonedHero.firstName} ${this.clonedHero.lastName}`
+        : '';
     },
   },
   methods: {
@@ -106,7 +114,7 @@ export default {
     },
   },
   watch: {
-    'hero.capeCounter': {
+    'clonedHero.capeCounter': {
       immediate: true,
       handler(newValue, oldValue) {
         console.log(
