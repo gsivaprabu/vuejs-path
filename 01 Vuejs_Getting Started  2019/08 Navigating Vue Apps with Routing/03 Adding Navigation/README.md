32d2d95f1b26ff672e8feef6397b89d1a155c3df
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2020-03-10 09:33:03 +0530

    routing link

--- 01 Vuejs_Getting Started  2019/08 Navigating Vue Apps with Routing/03 Adding Navigation/README.md
+++ 01 Vuejs_Getting Started  2019/08 Navigating Vue Apps with Routing/03 Adding Navigation/README.md
@@ -1,1 +1,18 @@
 # Adding Navigation
+
+## Routing in the template
+
+- The vue-cli  adds the <router-view> and some <router-link> components
+- Modify them as you need
+
+### aap.vue
+
+```html
+  <div id="app">
+    <div id="nav">
+        <router-link to="/">Villains<router-link>
+        <router-link to="/heroes">Heroes<router-link>
+    </div>
+    <router-view/> // Put that component that to navigate to here
+  </div>
+```  
--- 01 Vuejs_Getting Started  2019/pappa/08-routing/begin/vue-heroes/src/app.vue
+++ 01 Vuejs_Getting Started  2019/pappa/08-routing/begin/vue-heroes/src/app.vue
@@ -2,24 +2,21 @@
   <div id="app">
     <HeaderBar />
     <div class="main-section columns">
+      <NavBar />
       <main class="column">
         <router-view />
       </main>
     </div>
   </div>
 </template>
-
 <script>
 import HeaderBar from '@/components/header-bar';
-// import Heroes from '@/components/heroes';
-
+import NavBar from '@/components/navbar';
 export default {
   name: 'App',
-  // components: { HeaderBar, Heroes },
-  components: { HeaderBar },
+  components: { HeaderBar,NavBar}
 };
 </script>
-
 <style lang="scss">
 @import '@/design/index.scss';
 </style>
