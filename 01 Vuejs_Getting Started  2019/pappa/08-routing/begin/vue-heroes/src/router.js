70ae59797955abd976784e94160b2b1b298d684e
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2020-03-11 09:01:51 +0530

    Router Parameters and Props

--- 01 Vuejs_Getting Started  2019/pappa/08-routing/begin/vue-heroes/src/router.js
+++ 01 Vuejs_Getting Started  2019/pappa/08-routing/begin/vue-heroes/src/router.js
@@ -5,6 +5,7 @@ import About from './views/about.vue';
 import HeroDetail from './views/hero-detail.vue';
 
 Vue.use(Router);
+const parsePars = r =>({id:parseInt(r.params.id )})
 
 export default new Router({
   mode: 'history',
@@ -23,7 +24,7 @@ export default new Router({
       path: '/heroes/:id',
       name: 'hero-detail',
       component: HeroDetail,
-      props: r =>({id:parseInt(r.params.id )}),
+      props: parsePars,
     },
     {
       path: '/about',
